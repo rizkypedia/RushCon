1d4853a1c43d37efc03fa89e9b86d6889693c946
rizkyridwan <rizky.ridwan@trustedshops.de>
2020-12-07 18:29:45 +0100

    restructure App

--- App/Plugins/Onmeda/src/Controller/Component/SanteDrugs.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Onmeda\Controller\Component;
-
-class SanteDrugs {
-    public function __construct(){
-        echo "test";
-    }
-    
-}
-
--- App/Plugins/Onmeda/src/Model/config/MongoSource.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Onmeda\Model\Config;
-
-class MongoSource {
-    public function getSource() {
-       $connection = new \MongoClient();
-       return $connection;
-    }
-    
-}
