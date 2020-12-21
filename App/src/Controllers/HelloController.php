160dfcd55405e448ec39ccf1da24774f494b19d8
rizkyridwan <rizky.ridwan@trustedshops.de>
2020-12-21 16:32:02 +0100

    Last reformat

--- App/src/Controllers/HelloController.php
+++ App/src/Controllers/HelloController.php
@@ -6,6 +6,7 @@ namespace Rushcon\Controllers;
 use Rushcon\Core\BaseController;
 use Rushcon\Core\Console;
 use Rushcon\Core\Container;
+use Rushcon\Core\Services\LoggerService;
 
 class HelloController extends BaseController
 {
